aabe5d5c76ecbeab42c8867754232c85932e4ff6
shariarhossain23e <shariarhossain23@gamil.comm>
2022-08-03 15:42:02 +0600

    mongoDb setup

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+// userName database  :
+// password database: 
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Resume Builder Server');
+
+
+
+const uri = `mongodb+srv://${process.env.RESUME_BUILDER}:${process.env.RESUME_BUILDER_PASS}@cluster0.ozvnhci.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+// backend code
+async function run() {
+  try {
+   await client.connect()
+   console.log("db-connect");
+
+
+   const users = client.db("Resume_Builder").collection("user");
+    // for test
+    app.get('/users',async(req,res) => {
+        const result = await users.find().toArray()
+        res.send(result)
+    })
+    
+}
+
+finally {
+  }
+}
+run().catch(console.dir);
+
+// server run
+
+app.get("/", (req, res) => {
+  res.send("Resume Builder Server");
 });
 
-
-
 app.listen(port, () => {
-    console.log('Listening to port', port);
-})
+  console.log("Listening to port", port);
+});
