28462cb1fcb4844a5ccbacbde4ae260ff238d422
shariarhossain23e <shariarhossain23@gamil.comm>
2022-08-23 07:21:02 +0600

    user message

--- Socket/app.js
+++ Socket/app.js
@@ -23,12 +23,38 @@ const addUser = (userId, socketId) => {
     !users.some((user) => user.userId === userId) &&
       users.push({ userId, socketId });
   };
+
+  const getUser = (userId) => {
+    return users.find(user => user.userId === userId)
+  }
+
+  const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId)
+  }
 io.on("connection",(socket) => {
     console.log("on user connect");
+
+    // connect
     socket.on("addUser", (userId) => {
         addUser(userId, socket.id);
         socket.emit("getUsers", users);
       });
+
+      // send message
+      socket.on('sendMessage',({senderId,receiverId,text}) =>{
+        const user = getUser(receiverId,)
+        io.to(user.socketId).emit("getMessage",{
+          senderId,
+          text
+        })
+      })
+
+      // disconnect
+      socket.on("disconnect",()=> {
+        console.log("a user disconnect");
+        removeUser(socket.id)
+        socket.emit("getUsers", users);
+      })
 })
 server.listen(8000,() => {
     console.log("socket running");
